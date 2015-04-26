449e283b3de9db1f7d128f976b8b717262721083
Tor is Here <kittikorn.a@gmail.com>
2015-04-26 16:08:36 +0900

    Initial commit from local git

---
+++ benri/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  benri
+//
+//  Created by Kittikorn Ariyasuk on 4/26/15.
+//  Copyright (c) 2015 gobbl. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
