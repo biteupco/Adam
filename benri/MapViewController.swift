d31658eaefb1377e8687519d0213aa6183347cfe
TorIsHere <kittikorn.a@gmail.com>
2015-07-10 19:57:50 +0900

    Fix refresh route

--- benri/MapViewController.swift
+++ benri/MapViewController.swift
@@ -142,10 +142,14 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
                 
                 let myJSON = JSON(json!)
                 self.steps = myJSON["routes"][0]["legs"][0]["steps"].arrayValue
-                self.polyline = nil
+                
                 NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
                     if let encodePath = myJSON["routes"][0]["overview_polyline"]["points"].string {
                         var path:GMSPath = GMSPath(fromEncodedPath: encodePath)
+                        if self.polyline != nil {
+                            self.polyline.map = nil
+                        }
+                        
                         self.polyline = GMSPolyline(path: path)
                         self.polyline.strokeWidth = 5
                         self.polyline.strokeColor = UIColor.blueColor()
